bfe4956c017146704cd8068e0d4e69f7b23e1b78
Jan Kelemen <jkeleme@gmail.com>
2017-12-23 21:16:26 +0100

    add basic identity and entity models

---
+++ ca/src/ReqTrack/Domain.Core/Contexts/Entity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ReqTrack.Domain.Core.Contexts
+{
+    /// <summary>
+    /// <c>Entity</c> is a base class for all entities in the domain model.
+    /// </summary>
+    /// <typeparam name="T">Type of the entity.</typeparam>
+    public abstract class Entity<T> 
+    {
+        private Identity _id;
+
+        /// <summary>
+        /// Creates the entity.
+        /// </summary>
+        /// <param name="id">Entity identity.</param>
+        protected Entity(Identity id)
+        {
+            _id = id;
+        }
+
+        /// <summary>
+        /// Identity of the entity.
+        /// </summary>
+        public Identity Id => _id;
+
+        /// <summary>
+        /// Checks if a entity has blank identity.
+        /// </summary>
+        /// <returns><c>true</c> if a entity has blank identity.</returns>
+        public bool HasBlankIdentity() => _id.IsBlankIdentity();
+
+        /// <summary>
+        /// Checks if entities have same identity.
+        /// </summary>
+        /// <param name="other">other entity.</param>
+        /// <returns><c>true</c> if entities have same identity.</returns>
+        public bool HasSameIdentityAs(T other) => Equals(other);
+
+        /// <summary>
+        /// Checks if entities have same value, not including the identity.
+        /// </summary>
+        /// <param name="other">other entity.</param>
+        /// <returns><c>true</c> if entities have same value.</returns>
+        public virtual bool HasSameValueAs(T other)
+        {
+            throw new NotImplementedException("Probably YAGNI");
+        }
+
+        /// <inheritdoc />
+        public sealed override bool Equals(object obj)
+        {
+            var other = obj as Entity<T>;
+
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            return _id == other._id;
+        }
+
+        /// <inheritdoc />
+        public sealed override int GetHashCode() => _id.GetHashCode();
+
+        /// <inheritdoc />
+        public override string ToString() => $"ID={_id.ToString()}";
+    }
+}
---
+++ ca/src/ReqTrack/Domain.Core/Contexts/Identity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ReqTrack.Domain.Core.Contexts
+{
+    /// <summary>
+    /// <c>Identity</c> is a value type which represents the unique identity of the entity.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// Entities should always be compared by their identity, not by values. Blank identities always compare to false.
+    /// </remarks>
+    public struct Identity : IEquatable<Identity>
+    {
+        /// <summary>
+        /// Identity for temporary and non-persisted objects.
+        /// </summary>
+        public static readonly Identity BlankIdentity = new Identity(string.Empty);
+
+        /// <summary>
+        /// Factory method for creating identity of objects from string.
+        /// </summary>
+        /// <param name="value">identity value</param>
+        /// <returns>Unique entity identity.</returns>
+        /// <remarks>Usage of this method isn't recommended.</remarks>
+        public static Identity FromString(string value) => new Identity(value);
+
+        private readonly string _id;
+
+        private Identity(string id)
+        {
+            _id = id;
+        }
+
+        /// <summary>
+        /// Unique identifer.
+        /// </summary>
+        public string Id => _id;
+
+        /// <summary>
+        /// Checks if the identity is blank.
+        /// </summary>
+        /// <returns><c>true</c> if the identity is blank identity.</returns>
+        public bool IsBlankIdentity() => _id == string.Empty;
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is Identity && Equals((Identity) obj);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(Identity other) => !IsBlankIdentity() && !other.IsBlankIdentity() && (_id == other._id);
+
+        /// <inheritdoc />
+        public override int GetHashCode() => _id.GetHashCode();
+
+        /// <inheritdoc />
+        public override string ToString() => _id;
+
+        public static bool operator ==(Identity lhs, Identity rhs) => lhs.Equals(rhs);
+
+        public static bool operator !=(Identity lhs, Identity rhs) => !(lhs == rhs);
+
+        public static implicit operator Identity(string id)
+        {
+            return FromString(id);
+        }
+
+        public static implicit operator string(Identity id)
+        {
+            return id.ToString();
+        }
+    }
+}
