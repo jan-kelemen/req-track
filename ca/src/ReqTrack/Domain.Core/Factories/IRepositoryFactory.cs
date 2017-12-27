8febbcba6479749c8e5102983fdfaefe1d696c4a
Jan Kelemen <jkeleme@gmail.com>
2017-12-27 19:18:41 +0100

    add security gateway

---
+++ ca/src/ReqTrack/Domain.Core/Factories/IRepositoryFactory.cs
@@ -0,0 +1,15 @@
+﻿using ReqTrack.Domain.Core.Repositories;
+
+namespace ReqTrack.Domain.Core.Factories
+{
+    public interface IRepositoryFactory
+    {
+        IUserRepository UserRepository { get; }
+
+        IProjectRepository ProjectRepository { get; }
+
+        IRequirementRepository RequirementRepository { get; }
+
+        IUseCaseRepository UseCaseRepository { get; }
+    }
+}
---
+++ ca/src/ReqTrack/Domain.Core/Security/ISecurityGateway.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ReqTrack.Domain.Core.Entities;
+
+namespace ReqTrack.Domain.Core.Security
+{
+    public interface ISecurityGateway
+    {
+        IEnumerable<ProjectRights> GetAllProjectRights(Identity projectId);
+
+        ProjectRights GetProjectRights(Identity projectId, Identity userId);
+    }
+}
