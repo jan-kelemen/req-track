f096aa0c9a3665f18d6cfdb1c4c898fac83dacbd
Jan Kelemen <jkeleme@gmail.com>
2017-12-28 20:30:58 +0100

    initialize core usecases project

---
+++ ca/src/ReqTrack/Domain.Core.UseCases.Test.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Domain.Core.UseCases.Test.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
