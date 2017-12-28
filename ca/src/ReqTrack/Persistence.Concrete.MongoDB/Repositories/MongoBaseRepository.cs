40a2efc32558fc2fb2a44c6133675e52cba890f7
Jan Kelemen <jkeleme@gmail.com>
2017-12-28 13:43:44 +0100

    initialize mongo persistence project

---
+++ ca/src/ReqTrack/Persistence.Concrete.MongoDB/Repositories/MongoBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using ReqTrack.Domain.Core.Entities;
+using ReqTrack.Domain.Core.Repositories;
+
+namespace Persistence.Concrete.MongoDB.Repositories
+{
+    public abstract class MongoBaseRepository : IRepository
+    {
+        public Identity GenerateNewIdentity() => Identity.FromString(ObjectId.GenerateNewId().ToString());
+    }
+}
