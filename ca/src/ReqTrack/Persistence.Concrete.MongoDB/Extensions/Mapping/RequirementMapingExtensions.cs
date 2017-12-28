cebe275b635f7b5fa88c530d278dd00d601cd23f
Jan Kelemen <jkeleme@gmail.com>
2017-12-28 19:52:08 +0100

    add initial requirement repository implementation

--- ca/src/ReqTrack/Persistence.Concrete.MongoDB/Extensions/Mapping/RequirementMapingExtensions.cs
+++ ca/src/ReqTrack/Persistence.Concrete.MongoDB/Extensions/Mapping/RequirementMapingExtensions.cs
@@ -1,10 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using ReqTrack.Domain.Core.Entities.Projects;
+using ReqTrack.Domain.Core.Entities.Requirements;
+using ReqTrack.Domain.Core.Entities.Users;
+using ReqTrack.Persistence.Concrete.MongoDB.Entities;
 
 namespace ReqTrack.Persistence.Concrete.MongoDB.Extensions.Mapping
 {
     public static class RequirementMapingExtensions
     {
+        public static MongoRequirement ToMongoEntity(this Requirement requirement, int orderMarker = int.MaxValue)
+        {
+            return new MongoRequirement
+            {
+                Id = requirement.Id.ToMongoIdentity(),
+                AuthorId = requirement.Author.Id.ToMongoIdentity(),
+                ProjectId = requirement.Project.Id.ToMongoIdentity(),
+                Type = requirement.Type.ToString(),
+                Title = requirement.Title,
+                Note = requirement.Note,
+                OrderMarker = orderMarker,
+            };
+        }
+
+        public static Requirement ToDomainEntity(this MongoRequirement requirement, MongoUser author, MongoProject project)
+        {
+            return new Requirement(
+                requirement.Id.ToDomainIdentity(),
+                new BasicProject(project.Id.ToDomainIdentity(), project.Name),
+                new BasicUser(author.Id.ToDomainIdentity(), author.DisplayName),
+                Enum.Parse<RequirementType>(requirement.Type),
+                requirement.Title,
+                requirement.Note
+            );
+        }
     }
 }
--- ca/src/ReqTrack/Persistence.Concrete.MongoDB/Repositories/MongoRequirementRepository.cs
+++ ca/src/ReqTrack/Persistence.Concrete.MongoDB/Repositories/MongoRequirementRepository.cs
@@ -1,31 +1,101 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using ReqTrack.Domain.Core.Entities;
 using ReqTrack.Domain.Core.Entities.Requirements;
 using ReqTrack.Domain.Core.Repositories;
 using ReqTrack.Domain.Core.Repositories.Results;
+using ReqTrack.Persistence.Concrete.MongoDB.Entities;
+using ReqTrack.Persistence.Concrete.MongoDB.Extensions.Mapping;
 
 namespace ReqTrack.Persistence.Concrete.MongoDB.Repositories
 {
     public class MongoRequirementRepository : MongoBaseRepository, IRequirementRepository
     {
+        private readonly IMongoCollection<MongoRequirement> _requirements;
+
+        private readonly IMongoCollection<MongoProject> _projects;
+
+        private readonly IMongoCollection<MongoUser> _users;
+
+        public MongoRequirementRepository(
+            IMongoCollection<MongoRequirement> requirements,
+            IMongoCollection<MongoProject> projects,
+            IMongoCollection<MongoUser> users)
+        {
+            _requirements = requirements;
+            _projects = projects;
+            _users = users;
+        }
+
         public CreateResult<Requirement> CreateRequirement(Requirement requirement)
         {
-            throw new NotImplementedException();
+            var mongoRequirement = requirement.ToMongoEntity();
+
+            var project = FindByIdOrThrow(_projects, mongoRequirement.ProjectId);
+            var user = FindByIdOrThrow(_users, mongoRequirement.AuthorId);
+
+            mongoRequirement.OrderMarker = LastOrderMarker(project.Id, mongoRequirement.Type) + 1;
+
+            _requirements.InsertOne(mongoRequirement);
+
+            return new CreateResult<Requirement>(true, mongoRequirement.ToDomainEntity(user, project));
         }
 
         public ReadResult<Requirement> ReadRequirement(Identity id)
         {
-            throw new NotImplementedException();
+            var mongoRequirement = FindByIdOrThrow(_requirements, id.ToMongoIdentity());
+            var mongoProject = FindByIdOrThrow(_projects, mongoRequirement.ProjectId);
+            var mongoUser = FindByIdOrThrow(_users, mongoRequirement.AuthorId);
+
+            return new ReadResult<Requirement>(true, mongoRequirement.ToDomainEntity(mongoUser, mongoProject));
         }
 
         public UpdateResult<Requirement> UpdateRequirement(Requirement requirement)
         {
-            throw new NotImplementedException();
+            var mongoRequirement = requirement.ToMongoEntity();
+
+            var mongoProject = FindByIdOrThrow(_projects, mongoRequirement.ProjectId);
+            var mongoUser = FindByIdOrThrow(_users, mongoRequirement.AuthorId);
+            var orderMarker = LastOrderMarker(
+                                  requirement.Project.Id.ToMongoIdentity(),
+                                  requirement.Type.ToString()
+                              ) + 1;
+
+            var requirementFilter = Builders<MongoRequirement>.Filter.Eq(x => x.Id, mongoRequirement.Id);
+            var updateDefinition = Builders<MongoRequirement>.Update
+                .Set(x => x.AuthorId, mongoUser.Id)
+                .Set(x => x.Type, mongoRequirement.Type)
+                .Set(x => x.Title, mongoRequirement.Title)
+                .Set(x => x.Note, mongoRequirement.Note)
+                .Set(x => x.OrderMarker, orderMarker);
+            var options = new FindOneAndUpdateOptions<MongoRequirement>
+            {
+                ReturnDocument = ReturnDocument.After,
+            };
+
+            var updated = _requirements.FindOneAndUpdate(requirementFilter, updateDefinition, options);
+
+            return new UpdateResult<Requirement>(true, updated.ToDomainEntity(mongoUser, mongoProject));
         }
 
         public DeleteResult<Identity> DeleteRequirement(Identity id)
         {
-            throw new NotImplementedException();
+            var filter = Builders<MongoRequirement>.Filter.Eq(x => x.Id, id.ToMongoIdentity());
+            var entity = _requirements.FindOneAndDelete(filter);
+            return new DeleteResult<Identity>(true, entity.Id.ToDomainIdentity());
+        }
+
+        private int LastOrderMarker(ObjectId projectId, string type)
+        {
+            var projectFilter = Builders<MongoRequirement>.Filter.Eq(x => x.ProjectId, projectId);
+            var typeFilter = Builders<MongoRequirement>.Filter.Eq(x => x.Type, type);
+            var lastRequirement = _requirements
+                .Find(projectFilter & typeFilter)
+                .SortByDescending(x => x.OrderMarker) //https://stackoverflow.com/a/41503983 perfectly fine
+                .Limit(1)
+                .FirstOrDefault();
+
+            return lastRequirement?.OrderMarker ?? -1;
         }
     }
 }
