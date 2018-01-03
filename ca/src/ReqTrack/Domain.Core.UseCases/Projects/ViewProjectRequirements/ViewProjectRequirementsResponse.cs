a8f5627d577ffc36cd57c30c53b21d1ac7654554
Jan Kelemen <jkeleme@gmail.com>
2018-01-03 19:25:58 +0100

    implement view requirements by type use case

---
+++ ca/src/ReqTrack/Domain.Core.UseCases/Projects/ViewProjectRequirements/ViewProjectRequirementsResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ReqTrack.Domain.Core.UseCases.Boundary;
+using ReqTrack.Domain.Core.UseCases.Boundary.Responses;
+
+namespace ReqTrack.Domain.Core.UseCases.Projects.ViewProjectRequirements
+{
+    public class ViewProjectRequirementsResponse : ResponseModel
+    {
+        internal ViewProjectRequirementsResponse(ExecutionStatus status) : base(status)
+        {
+        }
+
+        public string ProjectId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public IEnumerable<Requirement> Requirements { get; set; }
+    }
+}
---
+++ ca/src/ReqTrack/Domain.Core.UseCases/Projects/ViewProjectRequirements/ViewProjectRequirementsUseCase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using ReqTrack.Domain.Core.Entities.Requirements;
+using ReqTrack.Domain.Core.Exceptions;
+using ReqTrack.Domain.Core.Repositories;
+using ReqTrack.Domain.Core.Security;
+using ReqTrack.Domain.Core.UseCases.Boundary;
+using ReqTrack.Domain.Core.UseCases.Boundary.Extensions;
+using ReqTrack.Domain.Core.UseCases.Boundary.Interfaces;
+using ReqTrack.Domain.Core.UseCases.Boundary.Responses;
+using ReqTrack.Domain.Core.UseCases.Exceptions;
+using AccessViolationException = System.AccessViolationException;
+
+namespace ReqTrack.Domain.Core.UseCases.Projects.ViewProjectRequirements
+{
+    public class ViewProjectRequirementsUseCase : IUseCase<ViewProjectRequirementsRequest, ViewProjectRequirementsResponse>
+    {
+        private readonly ISecurityGateway _securityGateway;
+
+        private readonly IProjectRepository _projectRepository;
+
+        public ViewProjectRequirementsUseCase(ISecurityGateway securityGateway, IProjectRepository projectRepository)
+        {
+            _securityGateway = securityGateway;
+            _projectRepository = projectRepository;
+        }
+
+        public void Execute(IUseCaseOutput<ViewProjectRequirementsResponse> output, ViewProjectRequirementsRequest request)
+        {
+            try
+            {
+                request.ValidateAndThrowOnInvalid();
+
+                var rights = _securityGateway.GetProjectRights(request.ProjectId, request.RequestedBy);
+                if (!rights.CanViewProject)
+                {
+                    throw new AccessViolationException("Project doesn't exist or user has insufficient rights");
+                }
+
+                var project = _projectRepository.ReadProjectRequirements(request.ProjectId, Enum.Parse<RequirementType>(request.Type));
+
+                output.Accept(new ViewProjectRequirementsResponse(ExecutionStatus.Success)
+                {
+                    Name = project.Name,
+                    ProjectId = request.ProjectId,
+                    Type = request.Type,
+                    Requirements = project.Requirements.Select(r => new Requirement
+                    {
+                        Id = r.Id,
+                        Title = r.Title,
+                    }),
+                });
+            }
+            catch (RequestValidationException e)
+            {
+                output.Accept(new ValidationErrorResponse
+                {
+                    Message = $"Invalid request. {e.Message}",
+                    ValidationErrors = e.ValidationErrors,
+                });
+            }
+            catch (AccessViolationException e)
+            {
+                output.Accept(new FailureResponse
+                {
+                    Message = $"Insufficient rights. {e.Message}",
+                });
+            }
+            catch (EntityNotFoundException e)
+            {
+                output.Accept(new FailureResponse
+                {
+                    Message = $"Project not found. {e.Message}",
+                });
+            }
+            catch (Exception e)
+            {
+                output.Accept(new FailureResponse
+                {
+                    Message = $"Tehnical error happend. {e.Message}",
+                });
+            }
+        }
+    }
+}
