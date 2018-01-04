d5b768979ecac293288825875125a82728154d8e
Jan Kelemen <jkeleme@gmail.com>
2018-01-04 20:32:08 +0100

    add web app project

---
+++ ca/src/ReqTrack/Application.Web.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReqTrack.Application.Web.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
